d50f8d1ebef757c91aff389d1b7597eeb1322990
Jannes <git@flate.eu>
2019-07-09 22:59:14 +0200

    integrate angular into gradle

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webapp';
+  result = '';
+
+  constructor(private client: HttpClient) {
+
+  }
+
+  private sayHello(): void {
+    this.result = 'loading...';
+    this.client.get('/api/hello-world').subscribe(response => this.result = response as string);
+  }
 }
