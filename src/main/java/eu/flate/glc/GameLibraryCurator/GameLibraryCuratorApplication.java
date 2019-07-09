7f099af36b66e4d4fd9cab3674b20f943375d760
Jannes <git@flate.eu>
2019-07-09 22:23:57 +0200

    base structure (spring)

---
+++ src/main/java/eu/flate/glc/GameLibraryCurator/GameLibraryCuratorApplication.java
@@ -0,0 +1,13 @@
+package eu.flate.glc.GameLibraryCurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryCuratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryCuratorApplication.class, args);
+	}
+
+}
