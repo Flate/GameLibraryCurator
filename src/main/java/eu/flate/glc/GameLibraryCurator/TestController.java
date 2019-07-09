35b1acc0f3bef13036d5baf1baf7ec53a9ac7f98
Jannes <git@flate.eu>
2019-07-09 22:32:36 +0200

    add test controller

---
+++ src/main/java/eu/flate/glc/GameLibraryCurator/TestController.java
@@ -0,0 +1,16 @@
+package eu.flate.glc.GameLibraryCurator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping(path = "/api")
+public class TestController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}
+
