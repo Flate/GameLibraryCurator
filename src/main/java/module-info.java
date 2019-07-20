4bf534b6f2048512d2382c3260d0d082eee928ff
Jannes <git@flate.eu>
2019-07-21 00:24:12 +0200

    more wip

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module eu.flate.GameLibraryCurator {
     requires spring.boot;
     requires spring.boot.autoconfigure;
+    requires spring.context;
     requires spring.web;
+    requires spring.data.jpa;
+    requires postgresql;
+    requires spring.tx;
+    requires spring.beans;
+    requires java.sql;
     requires spring.webflux;
+    requires spring.jdbc;
+    requires spring.orm;
     requires jackson.annotations;
 
     exports eu.flate.glc.main;
