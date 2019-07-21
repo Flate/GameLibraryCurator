f8dc6d2f1d80e2b7f3a6db8468a92d21d762ed11
Jannes <git@flate.eu>
2019-07-21 22:33:10 +0200

    fixes

--- src/main/java/eu/flate/glc/steam/SteamApi.java
+++ src/main/java/eu/flate/glc/steam/SteamApi.java
@@ -1,15 +1,15 @@
 package eu.flate.glc.steam;
 
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 public class SteamApi {
 
-    private static final String API_URL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/" +
-                                          "?key=%s" +
-                                          "&steamid=%s" +
-                                          "&format=json";
-
-    private WebClient client;
-    private String    key;
+//    private static final String API_URL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/" +
+//                                          "?key=%s" +
+//                                          "&steamid=%s" +
+//                                          "&format=json";
+//
+//    private WebClient client;
+//    private String    key;
 
 }
