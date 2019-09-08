95c21afc7fbb5a8ad51e9de79077924243d2f998
Jannes 'Flate' R <git@flate.eu>
2019-09-08 18:29:42 +0200

    add rebuild-script

--- src/main/java/eu/flate/glc/api/model/SteamGame.java
+++ src/main/java/eu/flate/glc/api/model/SteamGame.java
@@ -1,15 +1,18 @@
 package eu.flate.glc.api.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "games")
 @Data
-public class SteamGame extends Game {
+public class SteamGame
+    extends Game {
 
     @Column(name = "steam_appid")
     private String steamAppId;
